7c6dea07ea0914b38cc1ae984713b938a03d2f3e
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-16 19:52:11 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,27 @@
 def unsafe?(speed)
+  unsafe_speed = < 40
+if unsafe_speed < 40
+  "unsafe"
+else "safe"
+end 
 
-end
 
 
 
-def not_safe?(speed)
-	
-end
+
+
+age = 1
+
+if age < 2
+	"baby"
+else
+	"not a baby"
+end 
+
+# def not_safe?(speed)
+#   speed < 40 || speed > 60
+# end
+# end
 	
 
 
